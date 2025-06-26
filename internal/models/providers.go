c09e907b08567e855aa2c0ad08b93fe203aedc3f
Ed Zynda <ezynda3@gmail.com>
2025-06-26 13:31:42 +0300

    reorg

--- internal/models/providers.go
+++ internal/models/providers.go
@@ -15,7 +15,7 @@ import (
 	"github.com/cloudwego/eino-ext/components/model/ollama"
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
-	"github.com/mark3labs/mcphost/internal/progress"
+	"github.com/mark3labs/mcphost/internal/ui/progress"
 	"github.com/ollama/ollama/api"
 	"google.golang.org/genai"
 
